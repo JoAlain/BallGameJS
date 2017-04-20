62beea4dbfe5d460147f93992249c8b5230db0c4
JoAlain <rjoealain@gmail.com>
2017-04-20 16:43:36 +0300

    Add files via upload

--- js/jeu.js
+++ js/jeu.js
@@ -2,7 +2,7 @@ window.addEventListener('load', init);
 let canvas, ctx, width, height;
 let tableauxDesBalles = []; 
 let tableauxDesBlocs = []; 
-
+var particles = [];
 function init() {
   
   initFPS();
@@ -33,6 +33,173 @@ function mainLoop(time) {
 	}
 	requestAnimationFrame(mainLoop);
 }
+function Particle ()
+  {
+    this.scale = 1.0;
+    this.x = 0;
+    this.y = 0;
+    this.radius = 20;
+    this.color = "#000";
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.scaleSpeed = 0.5;
+    this.useGravity = false;
+      
+    this.update = function(ms)
+    {
+      // shrinking
+          
+      this.scale -= this.scaleSpeed * ms / 1000.0;
+          
+      if (this.scale <= 0)
+      {
+              // particle is dead, remove it
+              removeFromArray(particles, this);
+              
+      }
+      
+      // moving away from explosion center
+      this.x += this.velocityX * ms/1000.0;
+      this.y += this.velocityY * ms/1000.0;
+          
+          // and then later come downwards when our
+      // gravity is added to it. We should add parameters 
+          // for the values that fake the gravity
+          if(this.useGravity) {
+              this.velocityY += Math.random()*4 +4;
+          }
+    };
+    
+    this.draw = function(ctx)
+    {
+      // translating the 2D context to the particle coordinates
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.scale(this.scale, this.scale);
+      
+      // drawing a filled circle in the particle's local space
+      ctx.beginPath();
+      ctx.arc(0, 0, this.radius, 0, Math.PI*2, true);
+      //ctx.closePath();
+      
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      
+      ctx.restore();
+    };
+  }
+
+  function randomFloat (min, max)
+  {
+    return min + Math.random()*(max-min);
+  }
+
+  function removeFromArray(array, object) {
+      var idx = array.indexOf(object);
+      if (idx !== -1) {
+          array.splice(idx, 1);
+      }
+      return array;
+  }
+  function createBasicExplosion(x, y)
+  {
+    // creating 4 particles that scatter at 0, 90, 180 and 270 degrees
+    for (var angle=0; angle<360; angle+=90)
+    {
+      var particle = new Particle();
+      
+      // particle will start at explosion center
+      particle.x = x;
+      particle.y = y;
+      
+      particle.color = "#FF0000";
+      
+      var speed = 50.0;
+      
+      // velocity is rotated by "angle"
+      particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
+      particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
+      
+      // adding the newly created particle to the "particles" array
+      particles.push(particle);
+    }
+  }
+  function createExplosion(x, y, color)
+  {
+    var minSize = 10;
+    var maxSize = 30;
+    var count = 10;
+    var minSpeed = 60.0;
+    var maxSpeed = 200.0;
+    var minScaleSpeed = 1.0;
+    var maxScaleSpeed = 4.0;
+    
+    for (var angle=0; angle<360; angle += Math.round(360/count))
+    {
+      var particle = new Particle();
+      
+      particle.x = x;
+      particle.y = y;
+      
+            // size of particle
+      particle.radius = randomFloat(1, 3);
+      
+      particle.color = color;
+      
+            // life time, the higher the value the faster particle 
+            // will die
+      particle.scaleSpeed = randomFloat(0.3, 0.5);
+          
+            // use gravity
+          particle.useGravity = true;
+      
+      var speed = randomFloat(minSpeed, maxSpeed);
+      
+      particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
+      particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
+      
+      particles.push(particle);
+    }
+    
+  }
+  // Delta = time between two consecutive frames,
+  // for time-based animation
+  function updateAndDrawParticules(delta) {
+     for (var i = 0; i < particles.length; i++) {
+      var particle = particles[i];
+      
+      particle.update(delta);
+      particle.draw(ctx);
+     }
+  }
+
+  function startDoubleExplosion(x, y) {
+    createExplosion(x, y, "#525252");
+    // On peut multiplier la densité en générant plusieurs 
+    // explositons de couleurs différentes...
+    //console.log("mandalo explosion");
+    createExplosion(x, y, "#FFA318");
+    //createExplosion(x, y, "green");
+    //sound.play('blast');
+
+  }
+
+  function timer(currentTime) {
+    var delta = currentTime - oldTime;
+    oldTime = currentTime;
+    return delta;
+ 
+  }
+  // SOUND WITH HOWLER JS
+  /*var sound = new Howl({
+    urls: ['audio/sounds.mp3', 'audio/sounds.ogg'],
+    sprite: {
+      blast: [0, 2000],
+      laser: [3000, 700],
+      winner: [5000, 9000]
+    }
+  });*/
+
 function fillColorRange(range){
 	let color = "white";
 	switch(range){
@@ -121,6 +288,7 @@ function testeCollisionBalleAvecMurs(b) {
   }
   if(((b.y - b.rayon) < 75) && b.couleur=="red") {
     jeu.gameOver();
+    createBasicExplosion(b.x, b.y);
   }
 }
 function testCollisionBalleAvecBlocs(b, indexBalle) {
@@ -130,7 +298,16 @@ function testCollisionBalleAvecBlocs(b, indexBalle) {
   	  b.vy = -b.vy;
   	  if(b.couleur=="red"){
   	  	tableauxDesBalles.splice(indexBalle, 1);
+        startDoubleExplosion(b.x, b.y);
         creerDesBalles(2);
+        //startDoubleExplosion();
+        delta = timer(100000);
+       
+        // Clear canvas
+        //context2D.clearRect(0, 0, canvas.width, canvas.height);
+    
+        // Move and draw particles
+        updateAndDrawParticules(delta);
   	  }
     }
   });
